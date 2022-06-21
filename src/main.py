9c0950877fb19cfb4900a80e94303af03aee25b5
Sohams <s.shinde@student.saedubai.com>
2022-06-21 11:45:26 +0400

    Character stats

---
+++ src/main.py
@@ -0,0 +1,15 @@
+Char_1="Death Wizard"
+Health_1 = 1000
+Shield_1 = 500
+Stamina_1 = 100
+DMG_1 = 300
+Spirit_1 = 700
+
+
+print("CHARACER STATS: " + Char_1)
+print("Health: " + str(Health_1))
+print("Shield: " + str(Shield_1))
+print("Stamina: " + str(Stamina_1))
+print("DMG_1: " + str(DMG_1))
+print("Spirit_1: " + str(Spirit_1))
+
