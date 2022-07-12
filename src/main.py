1362dea98a41cdbcd28d6df0266020eaae17a738
Sohams <s.shinde@student.saedubai.com>
2022-07-12 13:17:38 +0530

    Drag racing game
    I passed the elements through an array

--- src/main.py
+++ src/main.py
@@ -1,25 +1,34 @@
 from turtle import Turtle, Screen 
 import random
 
+
 screen = Screen()
 screen.setup(1000, 800)
 screen.bgcolor("green")
 
-my_turtle = Turtle("turtle")
-my_turtle.color("red")
-my_turtle.shapesize(3,3)
-my_turtle.penup()
-my_turtle.goto(600, 0)
-my_turtle.position(10,5)
-my_turtle.speed(random.randint(1, 10))
+turtlelist=[ Turtle("turtle", visible=False), 
+Turtle("turtle", visible=False),
+Turtle("turtle", visible=False),
+Turtle("turtle", visible=False),
+Turtle("turtle", visible=False),
+Turtle("turtle", visible=False),
+Turtle("turtle", visible=False),
+Turtle("turtle", visible=False)
+]
+    
 
-my_turtle_2 = Turtle("turtle")
-my_turtle_2.color("yellow")
-my_turtle_2.shapesize(3,3)
-my_turtle_2.penup()
-my_turtle_2.goto(700,0)
-my_turtle_2.position()
-my_turtle_2.speed(random.randint(1, 10))
+
+for i in range(0, len(turtlelist)):
+    turtlelist[i].penup()
+    turtlelist[i].color("maroon")
+    turtlelist[i].shapesize(2,2)
+    turtlelist[i].goto(0, i * 50)
+    turtlelist[i].speed(random.randint(1, 10))
+    turtlelist[i].showturtle()
+    
+
+    print(turtlelist[i])
+
 screen.exitonclick()
 
 
