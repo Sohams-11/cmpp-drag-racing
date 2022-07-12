52372f83d6c5023a15788e5e0a2dbbac9b30449c
Sohams <s.shinde@student.saedubai.com>
2022-07-12 13:30:30 +0530

    Drag racing game

--- src/main.py
+++ src/main.py
@@ -23,12 +23,12 @@ for i in range(0, len(turtlelist)):
     turtlelist[i].color("maroon")
     turtlelist[i].shapesize(2,2)
     turtlelist[i].goto(0, i * 50)
-    turtlelist[i].speed(random.randint(1, 10))
     turtlelist[i].showturtle()
+    turtlelist[i].speed(random.randint(1, 10))
     turtlelist[i].setx(200)
     
 
-    print(turtlelist[i])
+  
 
 screen.exitonclick()
 
